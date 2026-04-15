b759a77a7962e6bff396f5248495994f5c050739
liqweb-gabriel <gabriel@liquidwebmasters.com>
2026-04-15 12:24:08 -0400

    Adicionar renderização dos posts em lista HTML

--- index.php
+++ index.php
@@ -14,3 +14,7 @@ $statement = $pdo->prepare("select * from posts");
 $statement->execute();
 
 $posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+  echo "<li>" . $post['title'] . "</li>";
+}
