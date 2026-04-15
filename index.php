539877874a2256d4981a52de5bdd7e7daeb49c48
liqweb-gabriel <gabriel@liquidwebmasters.com>
2026-04-15 12:23:55 -0400

    Adicionar conexão PDO com banco MySQL

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require "functions.php";
+
+// Connect do MySQL database
+
+$dsn = "mysql:host=localhost;port=3306;dbname=devops_pucpr;charset=utf8";
+$user = "root";
+$password ="MinhaSenhaSegura123";
+
+$pdo = new PDO($dsn, $user, $password);
